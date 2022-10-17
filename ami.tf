d79f2c9e25721796e191ae66f9e9736cf97e1108
Ravi <ravi@gmail.com>
2022-10-17 11:41:39 +0530

    ec2 code

--- ami.tf
+++ ami.tf
@@ -1,5 +1,5 @@
 resource "aws_ami_from_instance" "ami" {
   depends_on = [null_resource.ansible]
-  name               = local.TAG_PREFIX
+  name               = "${local.TAG_PREFIX}-${var.APP_VERSION}"
   source_instance_id = aws_instance.main.id
 }
